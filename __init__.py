0c756eb80bf689f4bbdb479dee4875312934ec19
Vasily Kokorev <vasilykokorev@gmail.com>
2012-12-25 23:35:13 +0400

    Изменился импорт с связи с переименованием datarr в clidataSet. Добавлен импорт geocalc.

--- __init__.py
+++ __init__.py
@@ -5,9 +5,6 @@ __author__ = 'Vasily Kokorev'
 __email__ = 'vasilykokorev@gmail.com'
 __version__ = '2.0 beta'
 
-from datarr import *
+from clidataSet import *
 import clicomp as cc
-
-#import altCli_inout
-#import altCli_visual
-#import altCli_select
+import geocalc
